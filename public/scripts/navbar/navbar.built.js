049b162998f7b79faccf1e7683b1407d5b03b1ad
luarug <luisarmandorubig@gmail.com>
2023-08-24 19:13:05 -0600

    luar
    ty

--- public/scripts/navbar/navbar.built.js
+++ public/scripts/navbar/navbar.built.js
@@ -1,12 +1,8 @@
 window.addEventListener("scroll", function(){
 
     var globalnav = document.querySelector(".globalnav");
-    var navFix = document.querySelector(".globalnav_fixed");
     var mobileglobalnav = document.querySelector(".mobile-globalnav");
-    var mobileNavFix = document.querySelector(".mobile-globalnav_fixed");
 
-    globalnav.classList.toggle("none_globalnav", window.scrollY > 0);
-    navFix.classList.toggle("globalnavbar_fixedblock", window.scrollY > 0);
-    mobileglobalnav.classList.toggle("none_mobile_globalnav", window.scrollY > 0);
-    mobileNavFix.classList.toggle("mobile_globalnavbar_fixedblock", window.scrollY > 0);
+    globalnav.classList.toggle("globalnav_fixed", window.scrollY > 0);
+    mobileglobalnav.classList.toggle("mobile_globalnavbar_fixed", window.scrollY > 0);
 });
